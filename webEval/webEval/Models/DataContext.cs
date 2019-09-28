ec95e25b05ce75446e9669d3405f642d12b662a5
New1 <New1@NEW>
2019-09-27 22:26:19 -0500

    datacontext

---
+++ webEval/webEval/Models/DataContext.cs
@@ -0,0 +1,13 @@
+﻿
+namespace webEval.Models
+{
+    using System.Data.Entity;
+
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+    }
+}
--- webEval/webEval/Models/Nota.cs
+++ webEval/webEval/Models/Nota.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
+﻿
 namespace webEval.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum Estado
     {
         Aprobado,
