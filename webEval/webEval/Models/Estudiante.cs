97815be8d2670a1504348b1ccb69ac28668979c1
New1 <New1@NEW>
2019-09-27 22:19:06 -0500

    modelos

---
+++ webEval/webEval/Models/Estudiante.cs
@@ -0,0 +1,26 @@
+﻿
+namespace webEval.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public enum Gender
+    {
+        Masculino,
+        Femenino
+
+    }
+    public class Estudiante
+    {
+        [Key]
+        [Required]
+        public int StudentID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Date { get; set; }
+        [Required]
+        public Gender gender { get; set; }
+        public virtual ICollection<Nota>Notas { get; set; }
+    }
+}
---
+++ webEval/webEval/Models/Nota.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webEval.Models
+{
+    public enum Estado
+    {
+        Aprobado,
+        Reprobado
+
+    }
+    public class Nota
+    {
+        [Key]
+        public int StudentID { get; set; }
+        [Required]
+        [Range(1,100)]
+        public int Calificacion { get; set; }
+        [Required]
+        public Estado estado { get; set; }
+        [Required]
+        public string Materia { get; set; }
+        public Estudiante Estudiante { get; set; }
+
+    }
+}
